b4385cddb158a91daa186de1276fcfed845f3a69
Jendrula95 <Jendrula95@hotmail.com>
2023-05-21 14:23:00 +0200

    Dodaj pliki projektów.

---
+++ RazorPagesBook/Migrations/20230521113254_Rating.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesBook.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Book");
+        }
+    }
+}
---
+++ RazorPagesBook/Pages/Models/SeedData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesBook.Data;
+
+namespace RazorPagesBook.Pages.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesBookContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesBookContext>>()))
+            {
+                if (context == null || context.Book == null)
+                {
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                }
+
+                // Look for any movies.
+                if (context.Book.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Book.AddRange(
+                    new Book
+                    {
+                        Title = "Władca Pierścieni",
+                        Genre = "Fantasy",
+                        Price = 8.99M,
+                         Rating = "S"
+                    },
+
+                    new Book
+                    {
+                        Title = "When Harry Met Sally",
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                         Rating = "A"
+                    },
+
+                    new Book
+                    {
+                        Title = "Pan Lodowego Ogrodu",
+                        Genre = "Fantasy",
+                        Price = 7.99M,
+                         Rating = "A"
+                    },
+
+                    new Book
+                    {
+                        Title = "Przemineło z Wiatrem",
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "A"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
